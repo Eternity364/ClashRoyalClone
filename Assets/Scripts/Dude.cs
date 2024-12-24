bac67ca3944191d0e4b7529646ee651291bc5153
igor.korobka <eternity364@gmail.com>
2024-12-24 08:21:08 +0300

    Added DOTween and shooting mechanic

--- Assets/Scripts/Dude.cs
+++ Assets/Scripts/Dude.cs
@@ -1,10 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Events;
 
 public class Dude : MonoBehaviour
 {    
+
     [SerializeField]
     int health;
     [SerializeField]
@@ -15,13 +18,23 @@ public class Dude : MonoBehaviour
     float attackRate;
     [SerializeField]
     private int team;
-    private NavMeshAgent navMeshAgent;
-    private Transform destination;
-    private Dude attackTarget;
-    private float timePassedSinceLastAttack;
 
     public float AttackRange => attackRange;
     public int Team => team;
+
+    public bool HasTarget
+    {
+        get
+        {
+            return attackTarget != null;
+        }
+    }
+    public UnityAction<Dude> OnDeath;
+
+    private NavMeshAgent navMeshAgent;
+    private Transform destination;
+    private Dude attackTarget;
+    private float timePassedSinceLastAttack = 0;
 
     void Awake() {
         navMeshAgent = GetComponent<NavMeshAgent>();
@@ -35,9 +48,48 @@ public class Dude : MonoBehaviour
     public void SetAttackTarget(Dude dude) {
         attackTarget = dude;
         navMeshAgent.isStopped = true;
+        attackTarget.OnDeath += ClearAttackTarget;
+        transform.LookAt(attackTarget.transform);
+        timePassedSinceLastAttack = 0;
+    } 
+    
+    public void ClearAttackTarget(Dude _) {
+        if (attackTarget != null) {
+            attackTarget.OnDeath -= ClearAttackTarget;
+            attackTarget = null;
+        }
+        navMeshAgent.isStopped = false;
+    } 
+
+    public void ReceiveAttack(int damage) {
+        health -= damage;
+        StartAttackAnimation();
+        if (health <= 0)
+            PerformDeath();
+    } 
+
+    private void PerformDeath() {
+        ClearAttackTarget(null);
+        navMeshAgent.isStopped = true;
+        OnDeath?.Invoke(this);
+        UnityEngine.Object.Destroy(gameObject);
     }
 
-    // void Update() {
-    //     navMeshAgent.destination = destination.position;
-    // }
+    private void StartAttackAnimation() {
+    }
+
+    private void PerformAttack() {
+        attackTarget.ReceiveAttack(attack);
+        print("attack");
+    }
+
+    void Update() {
+        if (attackTarget != null) {
+            timePassedSinceLastAttack += Time.deltaTime;
+            if (timePassedSinceLastAttack > attackRate) {
+                timePassedSinceLastAttack =- attackRate;
+                PerformAttack();
+            }
+        }
+    }
 }
--- Assets/TargetAcquiring.cs
+++ Assets/TargetAcquiring.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TargetAcquiring : MonoBehaviour
@@ -11,28 +12,54 @@ public class TargetAcquiring : MonoBehaviour
     [SerializeField]
     private float frequency = 0.2f;
     private float timePassed = 0;
+    private List<Dude> toRemove = new();
+    bool removeLock;
 
+    void Start() {
+        foreach (Dude dude in agents)
+        {
+            dude.OnDeath += RemoveUnit;
+        }
+    }
 
     public void AddUnit(Dude dude) {
         agents.Add(dude);
+        dude.OnDeath += RemoveUnit;
+    }
+
+    public void RemoveUnit(Dude dude) {
+        if (removeLock)
+            toRemove.Add(dude);
+        else {
+            agents.Remove(dude);
+        }
     }
 
     void Run() {
+        removeLock = true;
+        
+        print("agents count = " + agents.Count);
         for (int i = 0; i < agents.Count; i++)
         {
             Vector2 position = agents[i].transform.position;
             Dude agent = agents[i];
+            if (agent.IsDestroyed()) continue;
             int team = agent.Team;
             for (int j = 0; j < agents.Count; j++)
             {
+                if (agents[j].IsDestroyed()) continue;
                 if (i != j && team != agents[j].Team) {
                     float distance = (position - (Vector2)agents[j].transform.position).magnitude;
-                    print("dis = " + distance);
-                    if (agent.AttackRange > distance)
+                    if (!agent.HasTarget && agent.AttackRange > distance)
                         agent.SetAttackTarget(agents[j]);
                 }
             }
         }
+        removeLock = false;
+        for (int i = 0; i < toRemove.Count; i++) {
+            agents.Remove(toRemove[i]);
+        }
+        toRemove.Clear();
     }
 
     // Update is called once per frame
