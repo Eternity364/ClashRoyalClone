d19a39c3378a2e437f2604697866e6993fa845d2
igor.korobka <eternity364@gmail.com>
2025-06-10 18:30:47 +0300

    Added unit spawn animations and particles

--- Assets/Scripts/Unit/UnitButtonPanel.cs
+++ Assets/Scripts/Unit/UnitButtonPanel.cs
@@ -79,30 +79,7 @@ namespace Units {
 
         public void CreateFieldElixirAnimation(float value)
         {
-            GameObject icon = ObjectPool.Instance.GetObject(iconPrefab.gameObject);
-            Vector3 originalScale = icon.transform.localScale;
-            icon.transform.SetParent(parentForCopy);
-            icon.transform.localRotation = Quaternion.identity;
-            Vector3 hitPosition = panelArea.GetMouseHitPosition();
-            icon.transform.position = hitPosition;
-            icon.transform.localScale = new Vector3(originalScale.x * 0.5f, originalScale.y * 0.15f, originalScale.z);
-            var image = icon.GetComponent<Image>();
-            image.material = new Material(image.material);
-            image.material.color = new Color(image.material.color.r, image.material.color.g, image.material.color.b, 0);
-            TextMeshProUGUI costText = icon.GetComponentInChildren<TextMeshProUGUI>();
-            costText.text = "-" + value.ToString();
-
-            Sequence sequence = DOTween.Sequence();
-            sequence.Append(icon.transform.DOLocalMoveY(icon.transform.localPosition.y + 800f, 1f).SetEase(Ease.OutCirc));
-            sequence.Insert(0, icon.transform.DOScale(originalScale, 1.5f).SetEase(Ease.OutElastic));
-
-            sequence.Insert(0, image.material.DOFade(1, 0.4f).SetEase(Ease.OutCubic));
-            sequence.Insert(1.4f, image.material.DOFade(0, 0.4f).SetEase(Ease.InCubic));
-            sequence.Insert(1.501f, icon.transform.DOScale(Vector3.zero, 0.4f).SetEase(Ease.InCirc));
-            sequence.OnComplete(() => {
-                icon.transform.localScale = originalScale;
-                ObjectPool.Instance.ReturnObject(icon);
-            });
+            AnimationHelpers.CreateFieldElixirAnimation(iconPrefab.gameObject, parentForCopy, panelArea.GetMouseHitPosition(), value);
         }
 
         private void UpdateButtonsStatus(float elixirValue)
